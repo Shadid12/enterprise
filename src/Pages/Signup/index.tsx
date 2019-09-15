2ff601acfedf27fe127d0a3693ab3f9a6455642f
Shadid12 <Shadid12@users.noreply.github.com>
2019-09-15 18:52:33 -0400

    added more refactoring to cs

--- src/Pages/Signup/index.tsx
+++ src/Pages/Signup/index.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 import { ISignupPayloadModel } from '../../Models/ISignupPayloadModel';
 import { withFirebase } from '../../Containers/Firebase'
-
-// Styles 
-import { makeStyles } from '@material-ui/core/styles';
-import { Paper, Typography, Link, Container, CssBaseline, Avatar, TextField, FormControl, Button, Checkbox, Grid, Box } from '@material-ui/core';
+import { 
+    Typography, 
+    Link, 
+    Container, 
+    CssBaseline, 
+    Avatar, 
+    TextField,
+    Button, 
+    Grid, 
+    Box 
+} from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
+// styles 
+import useStyles from './styles'
 
 function Copyright() {
     return (
@@ -20,31 +29,6 @@ function Copyright() {
         </Typography>
     );
 }
-
-const useStyles = makeStyles(theme => ({
-    '@global': {
-        body: {
-            backgroundColor: theme.palette.common.white,
-        },
-    },
-    paper: {
-        marginTop: theme.spacing(10),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
-}));
 
 function SignupPageBase(props: any) {
 
