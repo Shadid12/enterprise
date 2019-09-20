90629615a79a025fbfa540b51440e148f2faee7e
haquesh <shadid.haque@cibc.com>
2019-09-20 15:59:18 -0400

    added scheduler

--- src/Pages/Scheduler/index.tsx
+++ src/Pages/Scheduler/index.tsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import AppointmentFormContainerBasic from '../../Components/Scheduler/index.js';
 
 export default class SchedulePage extends Component {
     render() {
         return (
             <div>
-                Hello worlds this is the schedule page
+                <h1>Title</h1>
+                <AppointmentFormContainerBasic />
             </div>
         )
     }
