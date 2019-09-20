10de9771323a293e48dda22ed75e0c3e98edc0f6
haquesh <shadid.haque@cibc.com>
2019-09-20 14:25:42 -0400

    ready to add scheduler compoent

---
+++ src/Pages/Scheduler/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SchedulePage extends Component {
+    render() {
+        return (
+            <div>
+                Hello worlds this is the schedule page
+            </div>
+        )
+    }
+}
